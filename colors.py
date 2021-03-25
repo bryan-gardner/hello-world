4ec5a27dbff77ff32700a5567d55ac86178992c1
bryan <bryan.gardner@gmail.com>
2021-03-25 15:27:24 -0700

    Modified file with 2 more colors, and age starting at 1.

--- colors.py
+++ colors.py
@@ -1,7 +1,6 @@
-colors = ['red', 'yellow', 'green', 'blue']
+colors = ['red', 'yellow', 'green', 'blue', 'orange', 'purple']
 
 i = 0
 while i < len(colors):
-    # print "When I was %d, my favorite color was %s" %  (i, colors[i])
-    print("When I was %d, my favorite color was %s" % (i, colors[i]))
+    print("When I was %d, my favorite color was %s" % (i + 1, colors[i]))
     i = i + 1
